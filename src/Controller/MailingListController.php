0c1029e3c538fb8a0d1db27cbb73501388865ae6
laurie.guibert <l.guibert@hotmail.fr>
2018-09-11 16:08:58 +0200

    crud mailing list

---
+++ src/Controller/MailingListController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\MailingList;
+use App\Form\MailingListType;
+use App\Repository\MailingListRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/mailing/list")
+ */
+class MailingListController extends Controller
+{
+    /**
+     * @Route("/", name="mailing_list_index", methods="GET")
+     */
+    public function index(MailingListRepository $mailingListRepository): Response
+    {
+        return $this->render('mailing_list/index.html.twig', ['mailing_lists' => $mailingListRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/new", name="mailing_list_new", methods="GET|POST")
+     */
+    public function new(Request $request): Response
+    {
+        $mailingList = new MailingList();
+        $form = $this->createForm(MailingListType::class, $mailingList);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($mailingList);
+            $em->flush();
+
+            return $this->redirectToRoute('mailing_list_index');
+        }
+
+        return $this->render('mailing_list/new.html.twig', [
+            'mailing_list' => $mailingList,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="mailing_list_show", methods="GET")
+     */
+    public function show(MailingList $mailingList): Response
+    {
+        return $this->render('mailing_list/show.html.twig', ['mailing_list' => $mailingList]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="mailing_list_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, MailingList $mailingList): Response
+    {
+        $form = $this->createForm(MailingListType::class, $mailingList);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('mailing_list_edit', ['id' => $mailingList->getId()]);
+        }
+
+        return $this->render('mailing_list/edit.html.twig', [
+            'mailing_list' => $mailingList,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="mailing_list_delete", methods="DELETE")
+     */
+    public function delete(Request $request, MailingList $mailingList): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$mailingList->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($mailingList);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('mailing_list_index');
+    }
+}
